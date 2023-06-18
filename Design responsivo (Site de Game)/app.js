f2752bddb209b5d17be819d4d1e13d475620031d
Samuel Caetano <127545215+saamxl@users.noreply.github.com>
2023-06-18 18:05:05 -0300

    Add files via upload
    🇧🇷 Site responsivo de uma loja de video games Xbox
    
    🇺🇸 Responsive website of an Xbox video game store

---
+++ Design responsivo (Site de Game)/app.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+const sobreElements = document.querySelectorAll('.sobre');
+sobreElements.forEach((el) => observer.observe(el));
